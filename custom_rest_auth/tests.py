73b6d6d6fbe0485fe2d74f9e977173abd1f7eaf3
Vadym Kaidalov <vadym.kaidalov@gmail.com>
2019-10-14 20:05:40 +0300

    Tests for ExpiringToken model

--- custom_rest_auth/tests.py
+++ custom_rest_auth/tests.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
@@ -8,11 +9,20 @@ USER_MODEL = get_user_model()
 
 
 class ExpiringTokenModelTests(TestCase):
-    def test_is_expired_with_new_token(self):
-        user = USER_MODEL.objects.create_user(
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = USER_MODEL.objects.create_user(
             username='hesoyam', password='postgres'
         )
 
-        expiring_token = ExpiringToken(user=user)
+    def test_is_expired_with_new_token(self):
+        expiring_token = ExpiringToken(user=self.user)
         expiring_token.save()
         self.assertFalse(expiring_token.is_expired())
+
+    def test_is_expired_with_expired_token(self):
+        expired_token = ExpiringToken(user=self.user)
+        expired_token.save()
+        expired_token.created -= settings.USER_TOKEN_LIFETIME
+        expired_token.save()
+        self.assertTrue(expired_token.is_expired())
